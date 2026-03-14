c41bfd4267c3acd970240e97582e69eaa0c5486e
Zheng Yang <zhengyangpon69@gmail.com>
2026-03-14 21:35:26 +0800

    same

--- security/oauth.py
+++ security/oauth.py
@@ -5,9 +5,11 @@ oauth = OAuth()
 oauth.register(
     name="google",
     client_id="761260701390-impffjtj4noo4bvcgn5ff1duvk6rr066.apps.googleusercontent.com",
-    #client_secret="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",
+    client_secret="idk"
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
     client_kwargs={
         "scope": "openid email profile"
     },
 )
+
+#client_secret="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",
